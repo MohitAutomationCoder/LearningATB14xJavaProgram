2d016ef63108009d93b8ed4a08a52b4cdadd070b
mohit-qa-admin <mohit.patel@azilen.com>
2025-09-13 11:51:03 +0530

    Session

---
+++ src/ex_02_Java_Basics/Lab005Comments_Multi.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basics;
+
+public class Lab005Comments_Multi {
+
+    /**
+     * Author : Pramod Dutta
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+
+    public static void main(String[] args) {
+        /**
+         * Author : Pramod Dutta
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+
+       /*
+
+        System.out.println("Heloo");
+        System.out.println("Heloo");
+
+        */
+
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab006_Keywords_Identifier.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics;
+
+public class Lab006_Keywords_Identifier {
+
+    public static void main(String[] args) {
+        System.out.println("Hi,Pramod");
+    }
+}
